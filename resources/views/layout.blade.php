e1288f65439d6a2c2e36e9eae54e1696de9fbfb1
nogto7 <nogto8888@gmail.com>
2026-05-04 16:39:04 +0800

    responsive

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -76,7 +76,6 @@
             </div>
         </div>
     </footer>
-    @include('components.toast')
 
     <script type="text/javascript" src="{{ asset('js/bootstrap.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('js/main.js') }}"></script>
